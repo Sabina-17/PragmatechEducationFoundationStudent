2d779b6330730c727f4b413e81a734b54b752787
Sabina-17 <sabina-sabina-19@bk.ru>
2021-07-15 16:50:42 +0400

    fayl

--- ProjectFrontEnd/js/app.js
+++ ProjectFrontEnd/js/app.js
@@ -4,3 +4,25 @@ function showBigImage(element) {
     let imgSrc = element.querySelector('img').getAttribute('src')
     bigImage.setAttribute('src', imgSrc)
 }
+
+
+
+
+
+
+
+
+
+function openTab(evt, TabName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(TabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
