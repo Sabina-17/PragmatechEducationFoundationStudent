fff8584c36fbde8056fd0fc5893a6f0342918216
Sabina-17 <sabina-sabina-19@bk.ru>
2022-02-20 17:28:28 +0400

    index

--- ProjectFrontEnd/MyPortfolio/JS/app.js
+++ ProjectFrontEnd/MyPortfolio/JS/app.js
@@ -1,4 +1,33 @@
 // Navbar
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+// function myFunction(x) {
+//     x.classList.toggle("change");
+//   }
+
+function deyis(){
+  document.querySelector(".navbar-nav").style.visibility="visible"
+  document.querySelector(".navbar-nav").style.opacity="1"
+  document.querySelector(".navbar-nav").style.transition="all 1s"
+  document.querySelector("#close").style.visibility="visible"
+  document.querySelector("#close").style.opacity="1"
+  document.querySelector("#open").style.visibility="hidden"
+  document.querySelector("#open").style.opacity="0"
+  
+
+
+}
+function bagla(){
+
+  document.querySelector(".navbar-nav").style.visibility="hidden"
+  document.querySelector(".navbar-nav").style.opacity="0"
+
+  document.querySelector("#close").style.visibility="hidden"
+  document.querySelector("#close").style.opacity="0"
+  document.querySelector("#open").style.visibility="visible"
+  document.querySelector("#open").style.opacity="1"
+ 
+  
+ }
+ function _border(){
+  document.querySelector(".navbar-nav span").style.borderColor="red"
+ }
+
